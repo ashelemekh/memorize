fe6dc6ad7043b03b486a805b6bbae5f3cc44cce3
Artem Shelemekh <a.shelemekh@gmail.com>
2020-06-25 18:33:00 +0300

    init commit

---
+++ Memorix/ContentView.swift
@@ -0,0 +1,44 @@
+//
+//  ContentView.swift
+//  Memorix
+//
+//  Created by Артем  on 20.06.2020.
+//  Copyright © 2020 Ashel. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        HStack {
+            ForEach(0..<4) { index in
+                CardView(isFaceUp: true)
+            }
+        }
+        .padding()
+        .foregroundColor(Color.orange)
+        .font(Font.largeTitle)
+    }
+}
+
+struct CardView: View {
+    var isFaceUp: Bool
+    var body: some View{
+        ZStack {
+            if isFaceUp {
+                RoundedRectangle(cornerRadius: 10.0).fill(Color.white)
+                RoundedRectangle(cornerRadius: 10.0).stroke(lineWidth: 3)
+                Text("👻")
+            } else {
+                RoundedRectangle(cornerRadius: 10.0).fill()
+            }
+        }
+    }
+}
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
